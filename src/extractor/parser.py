fe2cd7637e71ea9df371ec33380a923f744777af
Volodymyr Kuleshov <vol.kuleshov@gmail.com>
2016-08-08 12:16:30 -0700

    started moving text extraction to new snorkel, but currently it crashes; moving to tables directly

--- src/extractor/parser.py
+++ src/extractor/parser.py
@@ -13,10 +13,14 @@ class UnicodeXMLDocParser(XMLDocParser):
   breaks p-value formatting). 
   """
 
+  def __init__(self, path, doc='.//document', text='./text/text()', id='./id/text()',
+                    keep_xml_tree=False):
+    XMLDocParser.__init__(self, path, doc, text, id, keep_xml_tree)
+
   def parse_file(self, f, file_name):
     for i,doc in enumerate(et.parse(f).xpath(self.doc)):
       text = ' '.join(filter(lambda t : t is not None, doc.xpath(self.text)))
       ids = doc.xpath(self.id)
       id = ids[0] if len(ids) > 0 else None
       attribs = {'root':doc} if self.keep_xml_tree else {}
-      yield Document(id=str(id), file=str(file_name), text=unicode(text), attribs=attribs)
+      yield Document(name=str(id), file=str(file_name), attribs=attribs), unicode(text)

e4588bf6da886ee5391e421e357e1eb0fd4b8375
Volodymyr Kuleshov <vol.kuleshov@gmail.com>
2016-09-14 15:42:41 -0700

    extract mesh phenotypes from snorkel set

--- src/crawler/parse_snorkel_diseases.py
+++ src/crawler/parse_snorkel_diseases.py
@@ -23,30 +23,62 @@ def main():
   parser = argparse.ArgumentParser()
 
   parser.add_argument('--init', action='store_true', help='Init db')
-  parser.add_argument('--file', help='Load phenotypes')
+  parser.add_argument('--mesh', help='Mesh diseases')
+  parser.add_argument('--chemicals', help='Mesh chemicals')
   args = parser.parse_args()
 
   if args.init:
     init_db()
 
-  if args.file:
-    parse_ontology(args.file, db_session)
+  if args.mesh:
+    parse_mesh(args.mesh, db_session)
 
-def parse_ontology(fname, db_session):
+  if args.chemicals:
+    parse_chemicals(args.chemicals, db_session)  
+
+def parse_mesh(fname, db_session):
   with open(fname) as f:
     for line in f:
-      fields = line.strip().split(',')
-      name = _normalize_str(fields[1])
-      name = re.sub('"', '', name)
+      if line.startswith('#'): continue
+      
+      fields = line.strip().split('\t')
+      if len(fields) < 8: continue
+
+      name = _normalize_str(fields[0])
+      ontology_id = fields[1]
+      synonyms = _normalize_str(fields[7].lower())
 
       phenotype = Phenotype(
                     name=name,
-                    source='snorkel',
+                    source='mesh',
+                    synonyms=synonyms,
+                    ontology_ref=ontology_id
                   )
 
       db_session.add(phenotype)
   
   db_session.commit()
+
+def parse_chemicals(fname, db_session):
+  with open(fname) as f:
+    for line in f:
+      if line.startswith('#'): continue
+      
+      fields = line.strip().split('\t')
+
+      name = _normalize_str(fields[0])
+      ontology_id = fields[1]
+
+      phenotype = Phenotype(
+                    name=name,
+                    source='chemical',
+                    ontology_ref=ontology_id
+                  )
+
+      db_session.add(phenotype)
+  
+  db_session.commit()
+
 
 def _normalize_str(s):
   s = s.replace("\"", "") # remove quotes

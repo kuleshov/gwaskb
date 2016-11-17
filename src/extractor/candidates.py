b5596c6942e7d0b3ea8c7b991eaa4a59821831d1
Volodymyr Kuleshov <vol.kuleshov@gmail.com>
2016-11-16 20:09:17 -0800

    extend overlap fn to relations

--- src/extractor/candidates.py
+++ src/extractor/candidates.py
@@ -1,8 +1,8 @@
-def deduplicate(candidates):
+def deduplicate(candidates, i=0):
   # load existing candidates into a dict
-  span_dict = { str(cand.get_arguments()[0].parent) : list() for cand in candidates }
+  span_dict = { str(cand.get_arguments()[i].parent) : list() for cand in candidates }
   for cand in candidates:
-    span = cand.get_arguments()[0] # note: assumes this is not a relation
+    span = cand.get_arguments()[i] # note: assumes this is not a relation
     span_dict[str(span.parent)].append( (span.char_start, span.char_end) )
 
   def nested(ivl1, ivl2):
@@ -13,7 +13,7 @@ def deduplicate(candidates):
 
   new_candidates = list()
   for cand in candidates:
-      span = cand.get_arguments()[0] # note: assumes this is not a relation
+      span = cand.get_arguments()[i]
       span_ivl = span.char_start, span.char_end
       span_name = str(span.parent)
       if all([not nested(span_ivl, other_ivl) for other_ivl in span_dict[span_name]]):

2e95e3108862ca28d73841246eb4a0e5f0a23284
Volodymyr Kuleshov <vol.kuleshov@gmail.com>
2016-11-19 23:46:10 -0800

    update deduplication code

--- src/extractor/candidates.py
+++ src/extractor/candidates.py
@@ -1,9 +1,9 @@
 def deduplicate(candidates, i=0):
   # load existing candidates into a dict
-  span_dict = { str(cand.get_arguments()[i].parent) : list() for cand in candidates }
+  span_dict = { unicode(cand.get_arguments()[i].parent) : list() for cand in candidates }
   for cand in candidates:
     span = cand.get_arguments()[i] # note: assumes this is not a relation
-    span_dict[str(span.parent)].append( (span.char_start, span.char_end) )
+    span_dict[unicode(span.parent)].append( (span.char_start, span.char_end) )
 
   def nested(ivl1, ivl2):
     if ivl1 != ivl2 and ivl2[0] <= ivl1[0] <= ivl1[1] <= ivl2[1]:
@@ -15,7 +15,7 @@ def deduplicate(candidates, i=0):
   for cand in candidates:
       span = cand.get_arguments()[i]
       span_ivl = span.char_start, span.char_end
-      span_name = str(span.parent)
+      span_name = unicode(span.parent)
       if all([not nested(span_ivl, other_ivl) for other_ivl in span_dict[span_name]]):
           yield cand
 
